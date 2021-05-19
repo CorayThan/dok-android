434db9298515a9f8eb5bc6a6cf218a264c51a64b
Nathan Westlake <coraythan@gmail.com>
2021-05-19 01:02:02 -0700

    improvements

--- app/src/main/java/coraythan/decksofkeyforgeutilities/AddToDoKActivity.kt
+++ app/src/main/java/coraythan/decksofkeyforgeutilities/AddToDoKActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import coraythan.decksofkeyforgeutilities.ui.login.LoginActivity
@@ -13,7 +17,7 @@ class AddToDoKActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_find_user_or_redirect)
+        setContentView(R.layout.activity_add_to_dok)
 
         val authToken = this.appPrefs().findAuthToken()
 
@@ -42,21 +46,41 @@ class AddToDoKActivity : AppCompatActivity() {
 
             Log.i("add to dok", "Send request to send deck to DoK $it")
 
-            DoKService.addDeckToDoKAndMyDecks(authToken, this, it) { deckId, error ->
-                if (deckId != null) {
-                    Log.i("add to dok", "Added to DoK and called start on DoK URL $it")
+            importDeck(authToken, it)
+        }
+    }
 
-                    val viewIntent = Intent(
-                        "android.intent.action.VIEW",
-                        Uri.parse("https://decksofkeyforge.com/decks/$deckId")
-                    )
-                    startActivity(viewIntent)
-                } else {
-                    this.runOnUiThread {
-                        Toast.makeText(this, "Couldn't import and add the deck.", Toast.LENGTH_LONG).show()
+    private fun importDeck(authToken: String, deckUrl: String) {
+        DoKService.addDeckToDoKAndMyDecks(authToken, this, deckUrl) { deckId, error ->
+            if (deckId != null) {
+                Log.i("add to dok", "Added to DoK and called start on DoK URL $deckUrl")
+
+                val viewIntent = Intent(
+                    "android.intent.action.VIEW",
+                    Uri.parse("https://decksofkeyforge.com/decks/$deckId")
+                )
+                startActivity(viewIntent)
+            } else {
+                this.runOnUiThread {
+                    val addingDeckText = findViewById<TextView>(R.id.adding_deck).apply {
+                        setText(R.string.error_adding_deck)
                     }
-                    Log.e("add to dok", "Couldn't add due to error: $error" ?: "No error?!")
+                    val progressBar = findViewById<ProgressBar>(R.id.loading).apply {
+                        visibility = View.GONE
+                    }
+                    findViewById<Button>(R.id.retry).apply {
+                        visibility = View.VISIBLE
+
+                        setOnClickListener {
+                            importDeck(authToken, deckUrl)
+                            visibility = View.GONE
+
+                            progressBar.visibility = View.VISIBLE
+                            addingDeckText.setText(R.string.adding_deck)
+                        }
+                    }
                 }
+                Log.e("add to dok", "Couldn't add due to error: $error" ?: "No error?!")
             }
         }
     }
